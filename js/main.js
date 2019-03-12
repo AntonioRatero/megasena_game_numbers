d32fa5eb8472a3bab75a16e6b162fff2a44b4515
Antonio Ratero <aa.ratero@gmail.com>
2019-03-11 23:34:36 -0300

    Adicionado funcionalidades responsivas Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-function VerMensagem(){
-    alert("Hello world!");
-}
+//Animação
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".introducao h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao blockquote").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".introducao a").addClass("animated fadeInDown");
+	}, 1200);
+	setTimeout(function () {
+		$(".animar").addClass("animated fadeInDown");
+	}, 1600);	
+	setTimeout(function () {
+		$(".introducao-interna h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao-interna p").addClass("animated fadeInDown");
+	}, 800);		
+});	
