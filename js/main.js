c54b1dd65b5ab1dc1e0d77d4919ad52af8a9d960
ratero <aa.ratero@gmail.com>
2019-03-24 17:14:34 -0300

    Ddatatable settings

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@
 // Tabs Jquery
 
 $(document).ready(function(){
-	
 	$('ul.tabs li').click(function(){
 		var tab_id = $(this).attr('data-tab');
-
 		$('ul.tabs li').removeClass('current');
 		$('.tab-content').removeClass('current');
-
-		$('ul.tabs li').css("border-top","none")
-		$(this).css("border-top","2px solid #20986a")
-
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
 	})
-
 })
+
+// DataTable
+$(document).ready(function() {
+	$('#DataTable1, #DataTable2').DataTable({
+		"searching": false,
+		"language": {
+            "lengthMenu": "_MENU_ Jogos por página",
+            "zeroRecords": "Ops! Nenhum jogo encontrado...",
+            "info": "Vendo _PAGE_ de _PAGES_",
+			"infoEmpty": "Ops! Nenhum Jogo disponível...",
+			"paginate": {
+			"next": "Proxima",
+			"previous": "Anterior"
+			}
+		},
+		"infoFiltered": "(filtered from _MAX_ total records)",
+		responsive: true
+	});
+});
 
 //Animação
 
